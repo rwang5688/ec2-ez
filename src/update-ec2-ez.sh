e83e9056f7859052084b8430ba181cf217f552f2
Robert Wang <rwang5688@outlook.com>
2021-11-06 00:02:52 -0700

    add shell scripts and parameters files

---
+++ src/update-ec2-ez.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export EC2_EZ_SRC=$EC2_EZ_HOME/src
+
+# print environment variables
+echo "EC2_EZ_SRC=$EC2_EZ_SRC"
+
+# execute
+echo "update-ec2-ez instance_type=$1 region=$2 profile=$3 project=$4"
+aws cloudformation update-stack --stack-name ec2-ez-$1 \
+--template-body file://$EC2_EZ_SRC/ec2-ez.yaml \
+--capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+--parameters file://ec2-ez-$1-parameters.json \
+--region $2 \
+--profile $3 \
+--tags Key="Project",Value="$4"
